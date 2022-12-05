8656a2d45dff755078bc8f7c08f52f7ae6881c7c
Adegbusi Samuel <samuel.adegbusi@gmail.com>
2022-12-05 02:51:19 -0800

    Linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,7 +9,7 @@ int is_palindrome(listint_t **head)
 	unsigned int len = 1;
 	listint_t *temp;
 
-	if (head == NULL|| *head == NULL)
+	if (head == NULL || *head == NULL)
 		return (1);
 
 	temp = *head;
