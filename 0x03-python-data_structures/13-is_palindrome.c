78f8712824a06f973e233b4d639c104a9fb9618e
Adegbusi Samuel <samuel.adegbusi@gmail.com>
2022-12-05 03:09:21 -0800

    Linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,22 +1,37 @@
 #include "lists.h"
+
 /**
- * is_palindrome - checks if palindrome
- * @head: head of node
- * Return: 0 if not, 1 if is
+ * _is_palindrome - determine if a singly linked list is a palindrome (helper)
+ * @head: a pointer to a pointer to the start of the list
+ *
+ * Return: 1 if the list is a palindrome, otherwise 0
+ */
+int _is_palindrome(listint_t **head, listint_t *tail)
+{
+	int status = 1;
+
+	if (tail)
+	{
+		status = _is_palindrome(head, tail->next);
+
+		if (tail == *head || tail->next == *head)
+			*head = tail;
+		else if (status && (*head)->n == tail->n)
+			*head = (*head)->next;
+		else
+			status = 0;
+	}
+	return (status);
+}
+
+
+/**
+ * is_palindrome - determine if a singly linked list is a palindrome
+ * @head: a pointer to a pointer to the start of the list
+ *
+ * Return: 1 if the list is a palindrome, otherwise 0
  */
 int is_palindrome(listint_t **head)
 {
-	unsigned int len = 1;
-	listint_t *temp;
-
-	if (head == NULL || *head == NULL)
-		return (1);
-
-	temp = *head;
-	while (temp) /* get len of list */
-	{
-		temp = temp->next;
-		len++;
-	}
-	return (0);
+	return (head && _is_palindrome(head, *head));
 }
